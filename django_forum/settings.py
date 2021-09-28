1650b046687bc8191a47eada8ecb06465708900a
sajid Khurshid bhat <sajid@techis.io>
2021-09-28 22:31:16 +0530

    proc file

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'django_forum.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+    'default' : {
+       'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
