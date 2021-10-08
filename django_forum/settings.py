ce557c1ac6859df79d9a72d505597f85de619109
sajid Khurshid bhat <sajid@techis.io>
2021-10-09 05:11:24 +0530

    database

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'django_forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR , 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,12 +135,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 STATICFILES_DIRS = [
-    BASE_DIR , 'static',
+    BASE_DIR / 'static',
 ]
 
 
