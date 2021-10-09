f1db27198a6520e252d2379a9e0b1d57a9dc1491
sajid Khurshid bhat <sajid@techis.io>
2021-10-09 05:45:45 +0530

    database

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default' : {
        #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd5a35mamilkuta',
         'USER': 'kbviewbqnkeahk',
         'HOST': 'ec2-18-235-45-217.compute-1.amazonaws.com',
